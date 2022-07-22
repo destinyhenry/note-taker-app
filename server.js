f4b5ba71edb0e8c31d4e4ddf6d70fb7f004cbca0
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-22 08:57:20 -0400

    adding in middlewar and get routes

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express();
-const PORT = 3001;
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const note = require("./db/db.json");
+const uuid = require('./helpers/uuid');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// GET routes to connect to the html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/', (req, res) => res.json(note));
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
