a48a38c0fe7c83cf0e055cb21df056903443c491
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-22 09:25:55 -0400

    get note from db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const note = require("./db/db.json");
+const notes = require("./db/db.json");
 const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
@@ -15,12 +15,26 @@ app.use(express.static("public"));
 
 // GET routes to connect to the html
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/', (req, res) => res.json(note));
+app.get('/', (req, res) => res.json(notes));
+
+// get note from db
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to get notes`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
+
+// might try something with this
+// const uuid = () => {
+  // return Math.floor((1 + Math.random()) * 0x10000)
+  // .toString(16)
+  // .substring(1);
+// };
 
